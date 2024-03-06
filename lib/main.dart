79d7657a3e7d909fa4e21f5fd3182ee1180b096d
Syamsul <syamsulasyraf17@gmail.com>
2024-03-06 17:07:53 +0800

    apply to darkmode

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text("string"),
+        title: Text("word"),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
